c66fc9d64cea15a17d830c9ac99ad5f02f749357
Rolands <rolands.laucis@gmail.com>
2026-04-29 18:03:30 +0200

    upd

---
+++ experiments/colors.py
@@ -0,0 +1,9 @@
+from argparse import Namespace
+
+colors = Namespace(**{
+    'accuracy': "#4b8fd8",
+    'test_accuracy': "#4bd88d",
+    'loss': "#d84b4b",
+    'energy': "#fdf502",
+    'time': "#8fd84b"
+})
